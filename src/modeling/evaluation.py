86e8eea1e3cdfd413940df1e89f9bc971f2ea3db
angus saw <angussaw39@gmail.com>
2023-04-04 16:10:48 +0800

    Edits to generate_gradcam_visualizations

--- src/modeling/evaluation.py
+++ src/modeling/evaluation.py
@@ -66,10 +66,20 @@ class Evaluator:
         test_predictions = np.array([])
         test_labels =  np.array([])
         for x, y in test_data:
-            test_img_arrays.append(x)
+            test_img_arrays = test_img_arrays + tf.unstack(x)
             prediction_prob = self.model.predict(x).flatten()
             test_predictions = np.concatenate([test_predictions, tf.where(prediction_prob < 0.5, 0, 1)])
             test_labels = np.concatenate([test_labels, y.numpy()])
+
+        test_true_positives = np.where((test_labels == 1) & (test_predictions == 1))[0]
+        test_false_positives = np.where((test_labels == 0) & (test_predictions == 1))[0]
+        test_true_negatives = np.where((test_labels == 0) & (test_predictions == 0))[0]
+        test_false_negatives = np.where((test_labels == 1) & (test_predictions == 0))[0]
+
+        test_outcomes_dict = {"True Positives":test_true_positives,
+                              "False Positives": test_false_positives,
+                              "True Negatives": test_true_negatives, 
+                              "False Negatives": test_false_negatives}
         
         test_metrics = {
             "test_accuracy": accuracy_score(test_labels, test_predictions),
@@ -84,10 +94,22 @@ class Evaluator:
         file_save_path = f"{visualizations_save_dir}/{file_name}"
         plt.savefig(file_save_path, bbox_inches="tight")
 
-        plot_name = self.generate_gradcam_visualizations(img_arrays = test_img_arrays, labels = test_labels)
-        file_name = f"{plot_name}.png"
-        file_save_path = f"{visualizations_save_dir}/{file_name}"
-        plt.savefig(file_save_path)
+        for outcome in test_outcomes_dict:
+            sample_img_indexes = list(np.random.choice(test_outcomes_dict[outcome], size=9))
+            sample_test_img_arrays = [test_img_arrays[i] for i in sample_img_indexes]
+            sample_test_prediction_probabilities = [self.model.predict(np.expand_dims(img_array, axis=0))[0][0] for img_array in sample_test_img_arrays]
+            sample_test_predictions = test_predictions[sample_img_indexes]
+            sample_test_labels = test_labels[sample_img_indexes]
+            plot_name = f"Sample Grad-CAM visualizations on test data ({outcome})"
+
+            self.generate_gradcam_visualizations(plot_name = plot_name,
+                                                 sample_test_img_arrays = sample_test_img_arrays,
+                                                 sample_test_prediction_probabilities = sample_test_prediction_probabilities,
+                                                 sample_test_predictions = sample_test_predictions,
+                                                 sample_test_labels = sample_test_labels)
+            file_name = f"{plot_name}.png"
+            file_save_path = f"{visualizations_save_dir}/{file_name}"
+            plt.savefig(file_save_path)
 
         return final_metrics, test_metrics, visualizations_save_dir
     
@@ -106,7 +128,7 @@ class Evaluator:
         """
         # First, we create a model that maps the input image to the activations
         # of the last conv layer as well as the output predictions
-        self.model.layers[-1].activation = None
+        # self.model.layers[-1].activation = None
         grad_model = tf.keras.models.Model(
             [self.model.inputs], [self.model.get_layer(self.last_conv_layer_name).output, self.model.output]
         )
@@ -114,7 +136,7 @@ class Evaluator:
         # Then, we compute the gradient of the top predicted class for our input image
         # with respect to the activations of the last conv layer
         with tf.GradientTape(persistent=True) as tape:
-            last_conv_layer_output, preds = grad_model(img_array)
+            last_conv_layer_output, preds = grad_model(img_array, training=False)
             if pred_index is None:
                 pred_index = tf.argmax(preds[0])
             class_channel = preds[:, pred_index]
@@ -165,48 +187,38 @@ class Evaluator:
         return superimposed_img
 
     
-    def generate_gradcam_visualizations(self, img_arrays, labels):
+    def generate_gradcam_visualizations(self, plot_name, sample_test_img_arrays, sample_test_prediction_probabilities, sample_test_predictions, sample_test_labels):
         """_summary_
 
         Args:
-            img_arrays (_type_): _description_
-            labels (_type_): _description_
-            last_conv_layer_name (_type_): _description_
-        """
+            sample_test_img_arrays (_type_): _description_
+            sample_test_prediction_probabilities (_type_): _description_
+            sample_test_predictions (_type_): _description_
+            sample_test_labels (_type_): _description_
 
-        random_img_arrays = np.random.randint(99, size=9)
-        plot_name = "Sample Grad-CAM visualizations on test data"
+        Returns:
+            _type_: _description_
+        """
+        
         fig, axes = plt.subplots(3,3, figsize=(18,18))
         fig.suptitle(plot_name)
+        sample_test_predictions = np.where(sample_test_predictions == 1.0, "PNEUMONIA", "NORMAL")
+        sample_test_labels = np.where(sample_test_labels == 1.0, "PNEUMONIA", "NORMAL")
 
-        for i in range(len(random_img_arrays)):
+        for i in range(len(sample_test_img_arrays)):
 
-            img_array = np.expand_dims(img_arrays[0][i], axis=0)
-            prediction_prob = self.model.predict(img_array)
-            prediction = np.where(prediction_prob[0] < 0.5, "NORMAL", "PNEUMONIA")[0]
-            label = "PNEUMONIA" if labels[i] == 1.0 else "NORMAL"
+            img_array = np.expand_dims(sample_test_img_arrays[i], axis=0)
+            prediction_prob = sample_test_prediction_probabilities[i]
+            prediction = sample_test_predictions[i]
+            label = sample_test_labels[i]
 
             heatmap = self.make_gradcam_heatmap(img_array = img_array)
             heatmap = np.uint8(255 * heatmap)
             superimposed_img = self.generate_superimposed_image(img_array = img_array, heatmap = heatmap)
 
             axes[i//3, i%3].imshow(superimposed_img)
-            axes[i//3, i%3].set_title(f"Actual: {label}, Predicted: {prediction}\nProbability: {round(prediction_prob[0][0],4)}")
+            axes[i//3, i%3].set_title(f"Actual: {label}, Predicted: {prediction}\nProbability: {prediction_prob}")
             axes[i//3, i%3].axis('off')
-
-        return plot_name
-
-        
-
-
-
-
-
-            
-    
-
-    
-
 
 
         
