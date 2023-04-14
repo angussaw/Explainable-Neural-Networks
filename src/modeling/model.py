50cd4b93c7adfa08cf89c1c170c1eaa02e9cdc0a
angus saw <angussaw39@gmail.com>
2023-04-14 22:58:38 +0800

    Edited training_pipeline for fine tuning

--- src/modeling/model.py
+++ src/modeling/model.py
@@ -5,20 +5,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from typing import List
+import mlflow
+import os
 
 
-def build_model(model_params: dict,
-                model_architecture_params: dict, 
+def build_model(model_architecture_params: dict, 
                 rescale_params: dict,
-                data_augmentation: dict,
-                metrics: List):
+                data_augmentation: dict):
     """_summary_
 
     Args:
-        model_params (dict): _description_
+        model_architecture_params (dict): _description_
         rescale_params (dict): _description_
         data_augmentation (dict): _description_
-        metrics (List): _description_
 
     Returns:
         _type_: _description_
@@ -36,7 +35,7 @@ def build_model(model_params: dict,
     base_model.trainable = False
 
     pooling = tf.keras.layers.GlobalMaxPooling2D()(base_model.layers[-1].output)
-    dropout = tf.keras.layers.Dropout(model_params["dropout"])(pooling)
+    dropout = tf.keras.layers.Dropout(model_architecture_params["dropout"])(pooling)
 
     dense_output = tf.keras.layers.Dense(model_architecture_params["n_nodes"], activation="relu")(dropout)
     dense_output = tf.keras.layers.Dense(model_architecture_params["n_nodes"], activation="relu")(dense_output)
@@ -48,8 +47,38 @@ def build_model(model_params: dict,
     final_output = tf.keras.layers.Dense(3, activation="softmax")(dense_output)
     model = tf.keras.models.Model(inputs=inputs, outputs=final_output)
 
-    model.compile(loss=model_params["loss_function"],
-                    optimizer=model_params["optimizer"],
-                    metrics=metrics)
-    
     return model
+
+
+def retrieve_model(run_id: str,
+                   model_uri: str,
+                   model_name: str,
+                   destination_path: str):
+    """_summary_
+
+    Args:
+        run_id (str): _description_
+        model_uri (str): _description_
+        model_name (str): _description_
+        destination_path (str): _description_
+
+    Raises:
+        mlflow_error: _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    artifact_uri = f'mlflow-artifacts:/{run_id}/{model_uri}/artifacts/{model_name}'
+    try:
+        mlflow.artifacts.download_artifacts(
+            artifact_uri=artifact_uri, dst_path=destination_path
+        )
+    except Exception as mlflow_error:
+        raise mlflow_error
+    
+    model_file = os.path.split(artifact_uri)[1]
+    model = tf.keras.models.load_model(f'{destination_path}/{model_file}')
+
+    return model
+
--- src/modeling/training_pipeline.py
+++ src/modeling/training_pipeline.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import pandas as pd
-from src.modeling.model import build_model
+from src.modeling.model import build_model, retrieve_model
 from src.modeling.evaluation import Evaluator
 from src.utils import (
     timer,
@@ -27,11 +27,18 @@ def training_pipeline(config: DictConfig):
     Args:
         config (dict): _description_
     """
+    mode = os.getenv("MODE").lower()
     data_dir = config["files"]["data_dir"]
-    validation_split = config["training_params"]["validation_split"]
-    img_height = config["model"]["rescale_params"]["img_height"]
-    img_width = config["model"]["rescale_params"]["img_width"]
-    batch_size = config["training_params"]["batch_size"]
+    model_params = config["model"]["model_params"]
+    training_params = config["model"]["training_params"]
+    compile_params = config["model"]["compile_params"]
+    fine_tuning_params = config["fine_tuning"]
+
+    validation_split = training_params["validation_split"]
+    img_height = model_params["rescale_params"]["img_height"]
+    img_width = model_params["rescale_params"]["img_width"]
+    batch_size = training_params["batch_size"]
+
 
     logger.info("Generating image datasets...")
     training_dataset = tf.keras.utils.image_dataset_from_directory(directory=f'{data_dir}/train',
@@ -61,23 +68,39 @@ def training_pipeline(config: DictConfig):
                                                                 image_size=(img_height, img_width),
                                                                 batch_size = batch_size)
 
-    logger.info("Building Model...")
-    model = build_model(**config["model"])
+    logger.info("Intialise MLFlow...")
+    artifact_name, description_str = init_mlflow(config["mlflow"])
+
+    if mode == "training":
+        logger.info("Building Model...")
+        model = build_model(**model_params)
+
+    elif mode == "fine_tuning":
+        logger.info("Retrieving Model...")
+        model = retrieve_model(**fine_tuning_params["retrieve_params"])
+        model.trainable=True
+        for layer in model.layers[:fine_tuning_params["fine_tune_at"]]:
+            layer.trainable = False
+        bn_layers = list(filter(lambda x: isinstance(x, tf.keras.layers.BatchNormalization), model.layers))
+        for layer in bn_layers:
+            layer.trainable = False
+
+    model.compile(loss=compile_params["loss_function"],
+                  optimizer=tf.keras.optimizers.Adam(learning_rate=compile_params["learning_rate"]),
+                  metrics=config["metrics"])
+    
 
     logger.info("Training Model...")
     history = model.fit(training_dataset,
-                        epochs=config["training_params"]["n_epochs"],
+                        epochs=training_params["n_epochs"],
                         validation_data=validation_dataset)
     
     logger.info("Evaluating Model...")
     evaluator = Evaluator(model=model, test_data=test_dataset, history=pd.DataFrame(history.history))
-    final_metrics, test_metrics, visualizations_save_dir = evaluator.evaluate_model(metrics=config["model"]["metrics"])
+    final_metrics, test_metrics, visualizations_save_dir = evaluator.evaluate_model(metrics=config["metrics"])
 
-    logger.info("Intialise MLFlow...")
-    artifact_name, description_str = init_mlflow(config["mlflow"])
     with mlflow.start_run(
         run_name=config["mlflow"]["run_name"], description=description_str) as run:
-    
         logger.info("Starting MLFlow Run...")
 
         logger.info("Saving model and params...")
@@ -91,8 +114,8 @@ def training_pipeline(config: DictConfig):
         model_save_path = f'{model_dir}\{model_file_name}'
         model.save(model_save_path)
         mlflow.log_artifact(model_save_path)
-        mlflow.log_params(config['model']["model_params"])
-        mlflow.log_params(config["training_params"])
+        mlflow.log_params(compile_params)
+        mlflow.log_params(training_params)
 
         logger.info("Saving metrics and model performance...")
         mlflow.log_metrics(final_metrics)
